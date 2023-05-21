9ab3bee9e38642b35000500cef16e37c091bb020
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-05-21 16:53:06 +0700

    first commit

---
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/GarbagePickupApiApplication.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.garbagepickupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbagePickupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbagePickupApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irdaislakhuafa/garbagepickupapi/GarbagePickupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.garbagepickupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbagePickupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
