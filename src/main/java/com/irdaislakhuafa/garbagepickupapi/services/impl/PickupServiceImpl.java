a0c359d5c4b524278bc144b669946f51eeab778f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-07-18 12:34:07 +0700

    added logic to select courier based on shortest distance of last location (i mean last login location)

--- src/main/java/com/irdaislakhuafa/garbagepickupapi/services/impl/PickupServiceImpl.java
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/services/impl/PickupServiceImpl.java
@@ -4,18 +4,21 @@ import com.irdaislakhuafa.garbagepickupapi.exceptions.custom.BadRequestException
 import com.irdaislakhuafa.garbagepickupapi.exceptions.custom.DataAlreadyExists;
 import com.irdaislakhuafa.garbagepickupapi.helpers.DistanceCalculatorHelper;
 import com.irdaislakhuafa.garbagepickupapi.models.entities.Pickup;
+import com.irdaislakhuafa.garbagepickupapi.models.entities.User;
 import com.irdaislakhuafa.garbagepickupapi.models.gql.request.pickup.PickupRequest;
 import com.irdaislakhuafa.garbagepickupapi.repository.PickupRepository;
 import com.irdaislakhuafa.garbagepickupapi.repository.TrashTypeRepository;
 import com.irdaislakhuafa.garbagepickupapi.repository.UserRepository;
 import com.irdaislakhuafa.garbagepickupapi.services.PickupService;
 import com.irdaislakhuafa.garbagepickupapi.services.UserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.Set;
 
@@ -34,6 +37,55 @@ public class PickupServiceImpl implements PickupService {
     @Override
     public Optional<Pickup> save(Pickup request) {
         try {
+            /*
+             * added logic to select courier
+             */
+
+            // find all user with role "courier" and not deleted
+            final var listCourier = this.userRepository.findAllByRoleNameEqualsIgnoreCaseAndByIsDeleted("courier", false);
+            if (listCourier.isEmpty()) {
+                throw new BadRequestException("no registered courier");
+            }
+
+
+            // calculate distance between user from request pickup with each courier
+            @Data
+            class CourierDistance {
+                private User courier;
+                private double distance; // in KM
+            }
+
+            final var listCourierDistance = new ArrayList<CourierDistance>();
+            listCourier.forEach(courier -> {
+                final var userLastLocation = request.getUser().getLastLocation();
+                final var courierLastLocation = courier.getLastLocation();
+                final var distance = distanceCalculatorHelper.calculateDistance(
+                        DistanceCalculatorHelper.Distance.builder()
+                                .lat(userLastLocation.getLat())
+                                .lng(userLastLocation.getLng())
+                                .build(),
+                        DistanceCalculatorHelper.Distance.builder()
+                                .lat(courierLastLocation.getLat())
+                                .lng(courierLastLocation.getLng())
+                                .build()
+                );
+
+                final var courierDistance = new CourierDistance() {{
+                    setCourier(courier);
+                    setDistance(distance);
+                }};
+
+                listCourierDistance.add(courierDistance);
+            });
+
+            // sort ascending by field "distance"
+            listCourierDistance.sort((a, b) -> (int) (a.distance - b.distance));
+
+            // apply selected courier
+            final var selected = listCourierDistance.get(0);
+            request.setCourier(selected.getCourier());
+
+            // save request
             final var result = this.pickupRepository.save(request);
             return Optional.of(result);
         } catch (DataIntegrityViolationException e) {
@@ -48,25 +100,28 @@ public class PickupServiceImpl implements PickupService {
         return null;
     }
 
+    /**
+     * @param request is request body to create new pickup
+     * @return result this method doesn't specify the courier
+     */
     @Override
     public Pickup fromRequestToEntity(PickupRequest request) {
+        // find user by id and throw exception if not exists
         final var user = this.userRepository.findById(request.getUserId());
         if (user.isEmpty()) {
             throw new BadRequestException(String.format("user with id '%s' not found", request.getUserId()));
         }
 
+        // find trash type by id and throw exception if not exists
         final var trashType = this.trashTypeRepository.findById(request.getTrashTypeId());
         if (trashType.isEmpty()) {
             throw new BadRequestException(String.format("trash type with id '%s' not found", request.getTrashTypeId()));
         }
 
-//        TODO: added logic how to assign pickup to courier
-//        this.userRepository.find
-
+        // build result
         final var result = Pickup.builder()
                 .resi(this.appResiPrefix)
                 .user(user.get())
-//                .courier()
                 .status(request.getStatus())
                 .weight(request.getWeight())
                 .place(request.getPlace())
@@ -76,7 +131,7 @@ public class PickupServiceImpl implements PickupService {
                 .createdBy(this.userService.getCurrentUser().getId())
                 .build();
 
-        return null;
+        return result;
     }
 
     @Override
