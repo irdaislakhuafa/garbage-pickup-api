877a9de51e8a8556685856b50ea76ab9f026c4db
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2023-07-26 17:20:17 +0700

    use `register` instead of `save` for entity user on graphql schema and rest api uri (#11)
    * added spring boot validation dependency
    
    * rename save to register on graphql schema and rest api uri

---
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/controllers/restapi/UserController.java
@@ -0,0 +1,28 @@
+package com.irdaislakhuafa.garbagepickupapi.controllers.restapi;
+
+import com.irdaislakhuafa.garbagepickupapi.models.entities.User;
+import com.irdaislakhuafa.garbagepickupapi.models.gql.request.user.UserRequest;
+import com.irdaislakhuafa.garbagepickupapi.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = {"/users"})
+@RestController
+@RequiredArgsConstructor
+@Validated
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping(value = {"/register"})
+    public ResponseEntity<User> register(@RequestBody UserRequest request, Errors errors) {
+        final var user = this.userService.fromRequestToEntity(request);
+        final var result = this.userService.save(user);
+        return ResponseEntity.ok(result.get());
+    }
+}
