ad6061f2b6fea6947e5966150b1547840891ab3f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-07-27 04:02:55 +0700

    modify something

--- src/main/java/com/irdaislakhuafa/garbagepickupapi/controllers/rest/UserController.java
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/controllers/rest/UserController.java
@@ -2,27 +2,45 @@ package com.irdaislakhuafa.garbagepickupapi.controllers.rest;
 
 import com.irdaislakhuafa.garbagepickupapi.models.entities.User;
 import com.irdaislakhuafa.garbagepickupapi.models.gql.request.user.UserRequest;
+import com.irdaislakhuafa.garbagepickupapi.models.rest.response.RestResponse;
+import com.irdaislakhuafa.garbagepickupapi.services.RestValidationService;
 import com.irdaislakhuafa.garbagepickupapi.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping(value = {"api/rest/users"})
+import java.util.Map;
+
+@RequestMapping(value = {"/api/rest/users"})
 @RestController
 @RequiredArgsConstructor
-@Validated
 public class UserController {
     private final UserService userService;
+    private final RestValidationService restValidationService;
 
-    @PostMapping(value = {"/register"})
-    public ResponseEntity<User> register(@RequestBody UserRequest request, Errors errors) {
+    @Operation(
+            summary = "This endpoint used to register new user",
+            parameters = {
+                    @Parameter(name = "image", description = "The image field is used for the user's profile photo, this field is optional, you can leave it empty")
+            }
+    )
+    @PostMapping(value = {"/register"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<RestResponse<User, Map<String, Object>>> register(@RequestBody @ModelAttribute @Valid UserRequest request, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest()
+                    .body(RestResponse.<User, Map<String, Object>>builder()
+                            .errors(this.restValidationService.getListErrors(errors))
+                            .build());
+        }
         final var user = this.userService.fromRequestToEntity(request);
         final var result = this.userService.save(user);
-        return ResponseEntity.ok(result.get());
+        return ResponseEntity.ok(RestResponse.<User, Map<String, Object>>builder()
+                .data(result.get())
+                .build());
     }
 }
--- src/main/java/com/irdaislakhuafa/garbagepickupapi/exceptions/rest/RestGeneralExceptionHandler.java
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/exceptions/rest/RestGeneralExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.irdaislakhuafa.garbagepickupapi.exceptions.rest;
 
+import com.irdaislakhuafa.garbagepickupapi.exceptions.custom.BadRequestException;
 import com.irdaislakhuafa.garbagepickupapi.models.rest.response.RestResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConversionException;
@@ -10,18 +11,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class GeneralExceptionHandler {
+public class RestGeneralExceptionHandler {
     @ExceptionHandler(value = {HttpMessageConversionException.class})
     public ResponseEntity<?> invalidRequestFormat(HttpMessageConversionException e) {
-        final var messages = new HashMap<String, Object>() {{
+        final var errors = new HashMap<String, Object>() {{
             put("error", "your request body use invalid format, " + e.getMessage());
         }};
 
         final var body = RestResponse.<Object, Map<String, Object>>builder()
                 .data(null)
-                .messages(messages)
+                .errors(errors)
                 .build();
-        
+
+        final var result = ResponseEntity.badRequest().body(body);
+        return result;
+    }
+
+    @ExceptionHandler(value = {BadRequestException.class})
+    public ResponseEntity<?> badRequest(BadRequestException e) {
+        final var errors = new HashMap<String, Object>() {{
+            put("error", e.getMessage());
+        }};
+
+        final var body = RestResponse.<Object, Map<String, Object>>builder()
+                .data(null)
+                .errors(errors)
+                .build();
+
         final var result = ResponseEntity.badRequest().body(body);
         return result;
     }
