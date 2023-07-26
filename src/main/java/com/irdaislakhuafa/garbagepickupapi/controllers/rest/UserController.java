904ca37ce0d761d12c26c56fbe17a51170353374
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-07-27 04:19:14 +0700

    added rest api endpoint to login user and get jwt token

--- src/main/java/com/irdaislakhuafa/garbagepickupapi/controllers/rest/UserController.java
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/controllers/rest/UserController.java
@@ -1,8 +1,11 @@
 package com.irdaislakhuafa.garbagepickupapi.controllers.rest;
 
 import com.irdaislakhuafa.garbagepickupapi.models.entities.User;
+import com.irdaislakhuafa.garbagepickupapi.models.gql.request.user.UserLoginRequest;
 import com.irdaislakhuafa.garbagepickupapi.models.gql.request.user.UserRequest;
+import com.irdaislakhuafa.garbagepickupapi.models.gql.response.JwtTokenResponse;
 import com.irdaislakhuafa.garbagepickupapi.models.rest.response.RestResponse;
+import com.irdaislakhuafa.garbagepickupapi.services.AuthService;
 import com.irdaislakhuafa.garbagepickupapi.services.RestValidationService;
 import com.irdaislakhuafa.garbagepickupapi.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -22,6 +25,7 @@ import java.util.Map;
 public class UserController {
     private final UserService userService;
     private final RestValidationService restValidationService;
+    private final AuthService authService;
 
     @Operation(
             summary = "This endpoint used to register new user",
@@ -41,6 +45,20 @@ public class UserController {
         final var result = this.userService.save(user);
         return ResponseEntity.ok(RestResponse.<User, Map<String, Object>>builder()
                 .data(result.get())
+                .build());
+    }
+
+    @PostMapping(value = {"/login"}, consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<RestResponse<JwtTokenResponse, Map<String, Object>>> login(@RequestBody @Valid UserLoginRequest request, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest()
+                    .body(RestResponse.<JwtTokenResponse, Map<String, Object>>builder()
+                            .errors(this.restValidationService.getListErrors(errors))
+                            .build());
+        }
+        final var result = this.authService.login(request);
+        return ResponseEntity.ok(RestResponse.<JwtTokenResponse, Map<String, Object>>builder()
+                .data(result)
                 .build());
     }
 }
