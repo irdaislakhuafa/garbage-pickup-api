de687c63a150ac3360386b3109129f0011365be2
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-07-27 22:13:59 +0700

    added graphql schema and resolver for role query to find all roles and find role by id

--- src/main/java/com/irdaislakhuafa/garbagepickupapi/services/RoleService.java
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/services/RoleService.java
@@ -2,11 +2,13 @@ package com.irdaislakhuafa.garbagepickupapi.services;
 
 import com.irdaislakhuafa.garbagepickupapi.models.entities.Role;
 import com.irdaislakhuafa.garbagepickupapi.models.gql.request.role.RoleRequest;
+import com.irdaislakhuafa.garbagepickupapi.services.converter.CRUDService;
 import com.irdaislakhuafa.garbagepickupapi.services.converter.EntityConverterService;
 
-import java.util.Optional;
+import java.util.List;
 
-public interface RoleService extends EntityConverterService<Role, RoleRequest, Object> {
-    Optional<Role> save(Role request);
+public interface RoleService extends CRUDService<Role, String>, EntityConverterService<Role, RoleRequest, Object> {
+
+    List<Role> findAll();
 
 }
