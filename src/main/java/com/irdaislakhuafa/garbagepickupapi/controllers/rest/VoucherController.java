0faf4eeba51b9d23138b371dd11cc132d0d82d3d
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-07-27 18:00:37 +0700

    added rest api endpoint to save or create new voucher

---
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/controllers/rest/VoucherController.java
@@ -0,0 +1,44 @@
+package com.irdaislakhuafa.garbagepickupapi.controllers.rest;
+
+import com.irdaislakhuafa.garbagepickupapi.models.entities.Voucher;
+import com.irdaislakhuafa.garbagepickupapi.models.gql.request.voucher.VoucherRequest;
+import com.irdaislakhuafa.garbagepickupapi.models.rest.response.RestResponse;
+import com.irdaislakhuafa.garbagepickupapi.services.RestValidationService;
+import com.irdaislakhuafa.garbagepickupapi.services.VoucherService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = {"/api/rest/vouchers"})
+@RequiredArgsConstructor
+public class VoucherController {
+    private final VoucherService voucherService;
+    private final RestValidationService restValidationService;
+
+    @Operation(summary = "Used to save or create new voucher")
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<RestResponse<Voucher, Map<String, Object>>> save(@ModelAttribute @Valid VoucherRequest request, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest()
+                    .body(RestResponse.<Voucher, Map<String, Object>>builder()
+                            .errors(this.restValidationService.getListErrors(errors))
+                            .build());
+        }
+
+        final var voucher = this.voucherService.fromRequestToEntity(request);
+        final var result = this.voucherService.save(voucher);
+        return ResponseEntity.ok(RestResponse.<Voucher, Map<String, Object>>builder()
+                .data(result.get())
+                .build());
+    }
+}
--- src/main/java/com/irdaislakhuafa/garbagepickupapi/models/gql/request/voucher/VoucherRequest.java
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/models/gql/request/voucher/VoucherRequest.java
@@ -1,6 +1,9 @@
 package com.irdaislakhuafa.garbagepickupapi.models.gql.request.voucher;
 
 import com.irdaislakhuafa.garbagepickupapi.models.entities.utils.VoucherType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,10 +15,24 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 @SuperBuilder
 public class VoucherRequest {
+    @NotBlank(message = "cannot be blank")
+    @NotEmpty(message = "cannot be empty")
+    @NotNull(message = "cannot be null")
     private String title;
+
+    @NotBlank(message = "cannot be blank")
+    @NotEmpty(message = "cannot be empty")
+    @NotNull(message = "cannot be null")
     private String description;
+
     private MultipartFile image;
-    private int pointsNeeded;
+
+    @NotNull(message = "cannot be null")
+    private Integer pointsNeeded;
+
+    @NotNull(message = "cannot be null")
     private VoucherType type;
-    private int value;
+
+    @NotNull(message = "cannot be null")
+    private Integer value;
 }
