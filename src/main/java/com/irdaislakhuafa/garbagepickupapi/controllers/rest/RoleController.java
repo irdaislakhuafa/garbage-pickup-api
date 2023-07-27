5c7cf24aade61774238258c33097085f9f710b4f
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-07-27 22:49:12 +0700

    added rest api endpoint to find all roles

--- src/main/java/com/irdaislakhuafa/garbagepickupapi/controllers/rest/RoleController.java
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/controllers/rest/RoleController.java
@@ -5,15 +5,14 @@ import com.irdaislakhuafa.garbagepickupapi.models.gql.request.role.RoleRequest;
 import com.irdaislakhuafa.garbagepickupapi.models.rest.response.RestResponse;
 import com.irdaislakhuafa.garbagepickupapi.services.RestValidationService;
 import com.irdaislakhuafa.garbagepickupapi.services.RoleService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,6 +22,7 @@ public class RoleController {
     private final RoleService roleService;
     private final RestValidationService restValidationService;
 
+    @Operation(summary = "Used to sace or create new role")
     @PostMapping
     public ResponseEntity<RestResponse<Role, Map<String, Object>>> save(@RequestBody @Valid RoleRequest request, Errors errors) {
         if (errors.hasErrors()) {
@@ -31,11 +31,20 @@ public class RoleController {
                             .errors(this.restValidationService.getListErrors(errors))
                             .build());
         }
-        
+
         final var role = this.roleService.fromRequestToEntity(request);
         final var result = this.roleService.save(role);
         return ResponseEntity.ok(RestResponse.<Role, Map<String, Object>>builder()
                 .data(result.get())
+                .build());
+    }
+
+    @Operation(summary = "Used to find all roles")
+    @GetMapping
+    public ResponseEntity<RestResponse<List<Role>, Map<String, Object>>> findAll() {
+        final var results = this.roleService.findAll();
+        return ResponseEntity.ok(RestResponse.<List<Role>, Map<String, Object>>builder()
+                .data(results)
                 .build());
     }
 }
