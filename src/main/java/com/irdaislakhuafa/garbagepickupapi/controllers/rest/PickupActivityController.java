d4940a55e5afaf4070620a13867f38cf6d9906e7
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-07-28 17:54:41 +0700

    added endpoint to create or save new pickup activity

---
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/controllers/rest/PickupActivityController.java
@@ -0,0 +1,44 @@
+package com.irdaislakhuafa.garbagepickupapi.controllers.rest;
+
+import com.irdaislakhuafa.garbagepickupapi.models.entities.PickupActivity;
+import com.irdaislakhuafa.garbagepickupapi.models.gql.request.pickupactivity.PickupActivityRequest;
+import com.irdaislakhuafa.garbagepickupapi.models.rest.response.RestResponse;
+import com.irdaislakhuafa.garbagepickupapi.services.PickupActivityService;
+import com.irdaislakhuafa.garbagepickupapi.services.RestValidationService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = {"/api/rest/pickupActivity"})
+@RequiredArgsConstructor
+public class PickupActivityController {
+    private final RestValidationService restValidationService;
+    private final PickupActivityService pickupActivityService;
+
+    @Operation(summary = "Used to save or create new pickup activity")
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<RestResponse<PickupActivity, Map<String, Object>>> save(@RequestBody @Valid PickupActivityRequest request, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest()
+                    .body(RestResponse.<PickupActivity, Map<String, Object>>builder()
+                            .errors(this.restValidationService.getListErrors(errors))
+                            .build());
+        }
+
+        final var pickupActivity = this.pickupActivityService.fromRequestToEntity(request);
+        final var result = this.pickupActivityService.save(pickupActivity);
+        return ResponseEntity.ok(RestResponse.<PickupActivity, Map<String, Object>>builder()
+                .data(result.get())
+                .build());
+    }
+}
