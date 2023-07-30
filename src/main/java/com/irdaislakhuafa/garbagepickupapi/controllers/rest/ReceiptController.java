1464e905115a876f445d105435c27dfbc377f57b
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-07-30 12:49:17 +0700

    added rest api endpoint to save or create new receipt for pickup

---
+++ src/main/java/com/irdaislakhuafa/garbagepickupapi/controllers/rest/ReceiptController.java
@@ -0,0 +1,48 @@
+package com.irdaislakhuafa.garbagepickupapi.controllers.rest;
+
+import com.irdaislakhuafa.garbagepickupapi.models.entities.Receipt;
+import com.irdaislakhuafa.garbagepickupapi.models.gql.request.receipt.ReceiptRequest;
+import com.irdaislakhuafa.garbagepickupapi.models.rest.response.RestResponse;
+import com.irdaislakhuafa.garbagepickupapi.services.ReceiptService;
+import com.irdaislakhuafa.garbagepickupapi.services.RestValidationService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = {"/api/rest/receipts"})
+@RequiredArgsConstructor
+public class ReceiptController {
+    private final ReceiptService receiptService;
+    private RestValidationService restValidationService;
+
+    @Operation(summary = "used to save or create new receipt")
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<RestResponse<Receipt, Map<String, Object>>> save(@RequestBody @Valid ReceiptRequest request, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest()
+                    .body(RestResponse.<Receipt, Map<String, Object>>builder()
+                            .errors(this.restValidationService.getListErrors(errors))
+                            .build());
+        }
+
+        final var receipt = this.receiptService.fromRequestToEntity(request);
+        final var result = this.receiptService.save(receipt);
+        return ResponseEntity.ok(RestResponse.<Receipt, Map<String, Object>>builder()
+                .data(result.get())
+                .build());
+    }
+
+    // used to find receipt bu receipt id
+    // used to find receipt by pickup id
+
+}
